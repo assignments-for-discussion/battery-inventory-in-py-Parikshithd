71fe136efbb65677f849d2c8b34ae72a89ddb108
Parikshithd <97677075+Parikshithd@users.noreply.github.com>
2022-01-13 18:48:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 def count_batteries_by_usage(cycles):
   for i in cycles:
     if i < 400:
-    return ['lowCount'] += 1
-  elif cycles > 400 or cycles <= 919:
-    return ['mediumCount'] += 1
-  else:
-    return ['highCount"] += 1
+      return ['lowCount'] += 1
+    elif i > 400 or i <= 919:
+      return ['mediumCount'] += 1
+    else:
+      return ['highCount"] += 1
 
  
 
